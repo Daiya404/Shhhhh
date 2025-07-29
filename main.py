9f75fa3ccf81a41ecbc1cb8499072c025480a975
Daiya404 <dakshgotsandalphon@gmail.com>
2025-07-29 14:07:24 +0530

    Lunch time :AYAYA:

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ token_path = Path(__file__).parent / "token.txt"
 with open(token_path, "r", encoding="utf-8") as f:
     TOKEN = f.read().strip()
 
+print(f'My Brain aint braining')
+
 class Client(discord.Client):
     async def on_ready(self):
         print(f'Ok! Ok! I, {self.user}, am awake!')
         print('-----------')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith('Tika'):
+            await message.channel.send(f'Oh {message.author.mention}! Did you say my name?')
 
 intents = discord.Intents.default()
 intents.message_content = True
