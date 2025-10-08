ba1eaf5c36fd214b2c4d1f8801faa840661ea0cd
Daiya404 <dakshgotsandalphon@gmail.com>
2025-10-08 14:40:23 +0530

    wel well well

--- cogs/bot_admin.py
+++ cogs/bot_admin.py
@@ -2,74 +2,59 @@
 import discord
 from discord.ext import commands
 from discord import option
+from utils.checks import is_bot_admin # Import our central check
 
-# --- Decorator ---
-def is_bot_admin():
-    """
-    A custom check to see if the user is a bot admin.
-    Server administrators are always considered bot admins.
-    """
-    async def predicate(ctx: discord.ApplicationContext) -> bool:
-        # Guild owners and users with Administrator permission are always bot admins.
-        if ctx.author.guild_permissions.administrator:
-            return True
-
-        # Check the manually added list from our data file.
-        bot_admins_data = await ctx.bot.data_manager.get_data("bot_admins")
-        guild_admins = bot_admins_data.get(str(ctx.guild.id), [])
-
-        return ctx.author.id in guild_admins
-    return commands.check(predicate)
-
-# --- Cog ---
 class BotAdmin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # Create a slash command group for better organization
-    admin_group = discord.SlashCommandGroup(
-        "bot-admin",
-        "Commands to manage who can use Tika's admin commands.",
-        checks=[is_bot_admin()] # Apply the check to the entire group
-    )
+    # --- COMMAND 1: /bot-admin (Admin Only) ---
+    # The command name the user sees is "bot-admin".
+    @commands.slash_command(name="bot-admin", description="Add or remove a bot admin.")
+    @commands.check(is_bot_admin)
+    @option("action", description="The action to perform.", choices=["Add", "Remove"])
+    @option("user", discord.Member, description="The user to manage.")
+    # RENAMED: The internal function name no longer starts with 'bot_'.
+    async def manage_admins(self, ctx: discord.ApplicationContext, action: str, user: discord.Member):
+        
+        if action == "Add":
+            bot_admins_data = await self.bot.data_manager.get_data("bot_admins")
+            guild_id_str = str(ctx.guild.id)
 
-    @admin_group.command(name="add", description="Adds a user as a bot admin.")
-    @option("user", discord.Member, description="The user to grant admin permissions to.")
-    async def add(self, ctx: discord.ApplicationContext, user: discord.Member):
-        bot_admins_data = await self.bot.data_manager.get_data("bot_admins")
-        guild_id_str = str(ctx.guild.id)
+            if guild_id_str not in bot_admins_data:
+                bot_admins_data[guild_id_str] = []
 
-        # Initialize list if guild is not in the data yet
-        if guild_id_str not in bot_admins_data:
-            bot_admins_data[guild_id_str] = []
+            if user.id in bot_admins_data[guild_id_str]:
+                await ctx.respond(f"{user.display_name} is already a bot admin.", ephemeral=True)
+                return
 
-        if user.id in bot_admins_data[guild_id_str]:
-            return await ctx.respond(f"{user.display_name} is already a bot admin.", ephemeral=True)
+            bot_admins_data[guild_id_str].append(user.id)
+            await self.bot.data_manager.save_data("bot_admins", bot_admins_data)
+            await ctx.respond(f"Done. I will now recognize {user.mention} as a bot admin.", ephemeral=True)
 
-        bot_admins_data[guild_id_str].append(user.id)
-        await self.bot.data_manager.save_data("bot_admins", bot_admins_data)
-        await ctx.respond(f"Done. I will now recognize {user.mention} as a bot admin.")
+        elif action == "Remove":
+            bot_admins_data = await self.bot.data_manager.get_data("bot_admins")
+            guild_admins = bot_admins_data.get(str(ctx.guild.id), [])
 
-    @admin_group.command(name="remove", description="Removes a user as a bot admin.")
-    @option("user", discord.Member, description="The user to remove admin permissions from.")
-    async def remove(self, ctx: discord.ApplicationContext, user: discord.Member):
-        bot_admins_data = await self.bot.data_manager.get_data("bot_admins")
-        guild_admins = bot_admins_data.get(str(ctx.guild.id), [])
+            if user.id not in guild_admins:
+                await ctx.respond(f"{user.display_name} wasn't a bot admin to begin with.", ephemeral=True)
+                return
 
-        if user.id not in guild_admins:
-            return await ctx.respond(f"{user.display_name} wasn't a bot admin to begin with.", ephemeral=True)
+            guild_admins.remove(user.id)
+            await self.bot.data_manager.save_data("bot_admins", bot_admins_data)
+            await ctx.respond(f"Noted. {user.mention} is no longer a bot admin.", ephemeral=True)
 
-        guild_admins.remove(user.id)
-        await self.bot.data_manager.save_data("bot_admins", bot_admins_data)
-        await ctx.respond(f"Noted. {user.mention} is no longer a bot admin.")
-
-    @admin_group.command(name="list", description="Lists all current bot admins.")
+    # --- COMMAND 2: /bot-admin-list (Public) ---
+    # The command name the user sees is "bot-admin-list".
+    @commands.slash_command(name="bot-admin-list", description="Lists all current bot admins.")
+    # RENAMED: The internal function name no longer starts with 'bot_'.
     async def list_admins(self, ctx: discord.ApplicationContext):
         bot_admins_data = await self.bot.data_manager.get_data("bot_admins")
         guild_admins = bot_admins_data.get(str(ctx.guild.id), [])
 
         if not guild_admins:
-            return await ctx.respond("No delegated bot admins have been added. Only server admins can use my commands.", ephemeral=True)
+            await ctx.respond("No delegated bot admins have been added. Only server admins can use my commands.", ephemeral=True)
+            return
 
         description = "\n".join(f"- <@{user_id}>" for user_id in guild_admins)
         embed = discord.Embed(
@@ -78,6 +63,14 @@ class BotAdmin(commands.Cog):
             color=discord.Color.blurple()
         )
         await ctx.respond(embed=embed, ephemeral=True)
+        
+    # --- Error Handler ---
+    async def cog_command_error(self, ctx: discord.ApplicationContext, error: discord.DiscordException):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.respond("You don't have permission to do that.", ephemeral=True)
+        else:
+            print(f"An unhandled error occurred in BotAdmin cog: {error}")
+            await ctx.respond("Something went wrong on my end.", ephemeral=True)
 
 def setup(bot):
     bot.add_cog(BotAdmin(bot))
--- cogs/feature_manager.py
+++ cogs/feature_manager.py
@@ -2,7 +2,7 @@
 import discord
 from discord.ext import commands
 from discord import option
-from .bot_admin import is_bot_admin
+from utils.checks import is_bot_admin # <-- IMPORT FROM THE NEW LOCATION
 
 AVAILABLE_FEATURES = [
     "bot_admin",
@@ -16,9 +16,7 @@ class FeatureManager(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        """Load feature settings from file into the cache when the bot starts."""
         self.feature_cache = await self.bot.data_manager.get_data("feature_toggles")
-        # The loading message in tika.py is sufficient. No need for extra console chatter.
 
     def is_feature_enabled(self, guild_id: int, feature_name: str) -> bool:
         guild_settings = self.feature_cache.get(str(guild_id), {})
@@ -27,11 +25,8 @@ class FeatureManager(commands.Cog):
     async def feature_autocomplete(self, ctx: discord.AutocompleteContext) -> list[str]:
         return [feature for feature in AVAILABLE_FEATURES if feature.startswith(ctx.value.lower())]
 
-    @commands.slash_command(
-        name="feature",
-        description="Enable or disable bot features for this server.",
-        checks=[is_bot_admin()]
-    )
+    @commands.slash_command(name="feature", description="Enable or disable bot features for this server.")
+    @commands.check(is_bot_admin) # We use our check as a decorator here
     @option("feature", description="The feature to manage.", autocomplete=feature_autocomplete)
     @option("state", description="The new state for the feature.", choices=["On", "Off"])
     async def feature_manager(self, ctx: discord.ApplicationContext, feature: str, state: str):
@@ -49,7 +44,16 @@ class FeatureManager(commands.Cog):
 
         state_text = "ENABLED" if new_state_bool else "DISABLED"
         feature_text = feature.replace('_', ' ').title()
-        await ctx.respond(f"Fine. The **{feature_text}** feature is now **{state_text}**.")
+        await ctx.respond(f"Fine. The **{feature_text}** feature is now **{state_text}**.", ephemeral=True)
+    
+    # Add an error handler for the check
+    async def cog_command_error(self, ctx: discord.ApplicationContext, error: discord.DiscordException):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.respond("You don't have permission to do that.", ephemeral=True)
+        else:
+            # For other errors, we can log them or send a generic message
+            print(f"An unhandled error occurred in FeatureManager: {error}")
+            await ctx.respond("Something went wrong on my end.", ephemeral=True)
 
 def setup(bot):
     bot.add_cog(FeatureManager(bot))
